8c0658b2e832b5c110024e74771dcc5541873aa6
AntoniosBarotsis <antonis.barotsis@gmail.com>
2020-04-28 19:57:32 +0300

    first commit!

--- test.js
+++ test.js
@@ -6,17 +6,16 @@ const REPO = 'github.com/AntoniosBarotsis/covid19TimeSeriesJSON';
 
 const shell =  require('shelljs');
 const remote = `https://${USER}:${PASS}@${REPO}`;
-console.log(remote);
 
 
 // const simpleGit = require('simple-git')();
 // const simpleGitPromise = require('simple-git/promise')();
 
 require('simple-git')()
-     .add('.')
+     .add('./*')
      .commit("first commit!")
-     .addRemote('origin', remote)
-     .push(['-u', 'origin', 'master'], () => console.log('done'));
+     .addRemote('origin', 'https://github.com/AntoniosBarotsis/covid19TimeSeriesJSON.git')
+     .push('origin', 'master');
 
 
 // shell.exec('chmod +x test.sh')
