bebb2be31883eb767a8b54859d758b4e2da39121
AntoniosBarotsis <antonis.barotsis@gmail.com>
2020-04-28 20:16:58 +0300

    AAAAAAAAAAAAAAAAAAAA!

--- test.js
+++ test.js
@@ -1,27 +1,16 @@
 require('dotenv').config()
+const git = require('simple-git/promise')()
+const shell =  require('shelljs');
 
 const USER = process.env.GIT_USER;
 const PASS = process.env.GIT_PASS;
 const REPO = 'github.com/AntoniosBarotsis/covid19TimeSeriesJSON';
-
-const shell =  require('shelljs');
 const remote = `https://${USER}:${PASS}@${REPO}`;
 
+pushToGit().then(console.log('Done'))
 
-// const simpleGit = require('simple-git')();
-// const simpleGitPromise = require('simple-git/promise')();
-//a
-const git = require('simple-git/promise')()
-     
-git.add('./*')
-git.commit("AAAAAAAAAAAAAAAAAAAA!")
-git.push(remote, 'master')
-
-
-// shell.exec('chmod +x test.sh')
-
-// shell.exec('./test.sh')
-
-// shell.exec('git add .')
-// shell.exec('git commit -m "Update"')
-// shell.exec('git push')
+async function pushToGit() {
+    git.add('./*')
+    git.commit("AAAAAAAAAAAAAAAAAAAA!")
+    git.push(remote, 'master')  
+}
