4dcc78470e2795646f9382f4d9d4c33e9f45479f
AntoniosBarotsis <antonis.barotsis@gmail.com>
2020-04-28 19:49:45 +0300

    Update

--- test.js
+++ test.js
@@ -1,9 +1,23 @@
+require('dotenv').config()
+
+const USER = process.env.GIT_USER;
+const PASS = process.env.GIT_PASS;
+const REPO = 'github.com/AntoniosBarotsis/covid19TimeSeriesJSON';
+
 const shell =  require('shelljs');
+const remote = `https://${USER}:${PASS}@${REPO}`;
 
-shell.exec('chmod +x test.sh')
+require('simple-git')()
+.add('./*')
+.commit("Update")
+.addRemote('origin', 'some-repo-url')
+.push(['-u', 'origin', 'master'], () => console.log('done'));
 
-shell.exec('./test.sh')
 
-shell.exec('git add .')
-shell.exec('git commit -m "Update"')
-shell.exec('git push')
+// shell.exec('chmod +x test.sh')
+
+// shell.exec('./test.sh')
+
+// shell.exec('git add .')
+// shell.exec('git commit -m "Update"')
+// shell.exec('git push')
