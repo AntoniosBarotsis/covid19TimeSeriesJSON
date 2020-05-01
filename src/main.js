210f13007434cc7e9dcbed87755dd76161e71bf1
Tony <antonis.barotsis@gmail.com>
2020-05-01 22:54:42 +0300

    more logs

--- src/main.js
+++ src/main.js
@@ -7,5 +7,6 @@ console.log('Main running')
 
 cron.schedule('0 0 */2 * * *', () => { // Checks every 2 hours
     console.log('Starting CRON job')
-    shell.exec('./src/getLastUpdate.sh ')
+    shell.exec('./src/getLastUpdate.sh')
+    console.log('CRON job finished')
 })
