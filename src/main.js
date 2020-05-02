713dffb101d847ccd7fffa058025ebd69db93142
Tony <antonis.barotsis@gmail.com>
2020-05-02 15:26:15 +0300

    added change

--- src/main.js
+++ src/main.js
@@ -5,14 +5,7 @@ shell.exec('chmod +x ./src/getLastUpdate.sh')
 
 console.log('Main running')
 
-// cron.schedule('0 0 */2 * * *', () => { // Checks every 2 hours
-//     const date = new Date()
-//     console.log(`Starting CRON job at ${date.getHours()}:${date.getMinutes()}`)
-//     shell.exec('./src/getLastUpdate.sh')
-//     console.log('CRON job finished')
-// })
-
-cron.schedule('9 * * * * *', () => { // Checks every 2 hours
+cron.schedule('0 0 */2 * * *', () => { // Checks every 2 hours
     const date = new Date()
     console.log(`Starting CRON job at ${date.getHours()}:${date.getMinutes()}`)
     shell.exec('./src/getLastUpdate.sh')
